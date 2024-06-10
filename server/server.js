d73f0c0768f8804246b2a3c2e7e47ea1d152eab8
ABDERRAZZAQ Mars <abderrazzaqmars@ABDERRAZZAQs-MacBook-Pro.local>
2024-06-10 02:03:42 +0100

    Initial commit server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 8000
+
+const app = express();
+
+//Error_Handler
+//app.use(globalError);
+
+app.listen(port, (err) => {
+  if (!err) {
+    console.log(`Server started on port ${port}`)
+  } else {
+    console.log('Error 500 côté serveur')
+  }
+});
